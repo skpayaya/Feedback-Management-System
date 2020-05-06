32354c6e61c1b3b193949af4c5f7ac7b77f29950
skpayaya <sashi.kumar.patelskp@gmail.com>
2020-05-06 08:45:10 +0530

    first

---
+++ src/test/java/com/cg/iter/feedbackmanagementsystem/FeedbackManagementSprintApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cg.iter.feedbackmanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedbackManagementSprintApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
